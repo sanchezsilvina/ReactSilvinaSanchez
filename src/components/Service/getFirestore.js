cec583c250286db1c7eadd95a21ef27b26ead41d
silvina sanchez <silvinaasanchez@gmail.com>
2021-11-22 21:12:43 -0300

    Entregable clase 12

---
+++ src/components/Service/getFirestore.js
@@ -0,0 +1,20 @@
+import firebase  from 'firebase'; 
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSVN-uv5LyvHrJVWUDDzlLxUYJ_jIbR0o",
+  authDomain: "ecommercereact-2cbee.firebaseapp.com",
+  projectId: "ecommercereact-2cbee",
+  storageBucket: "ecommercereact-2cbee.appspot.com",
+  messagingSenderId: "1059454593959",
+  appId: "1:1059454593959:web:7c0b413a96ef396b53e482",
+  measurementId: "G-FE5JCR9XL1"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+export function getFirestone()
+{
+    return  firebase.firestore(app)
+}
